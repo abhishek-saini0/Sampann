1c12788f98311cf27f457b3b7af8af09030a5177
Abhishek saini <abhishek331saini@gmail.com>
2024-09-09 10:50:44 +0530

    done

--- models/db.js
+++ models/db.js
@@ -1,3 +1,25 @@
+// // config/sequelize.js
+// const { Sequelize } = require('sequelize');
+
+// // Use DATABASE_URL if provided, otherwise use individual env vars
+// const sequelize = new Sequelize(process.env.DATABASE_URL || {
+//   database: process.env.DB_NAME,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: true, // Enforces SSL for security
+//       rejectUnauthorized: false // Needed for self-signed certificates
+//     }
+//   }
+// });
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require('sequelize');
 const config = require('../config/config');
 
